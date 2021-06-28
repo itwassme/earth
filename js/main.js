565e12df7790d97739c384363c7eed7860934b48
Emily <itwassme@gmail.com>
2021-06-28 17:34:56 +0900

    made changes

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ $('.menu-toggle').on('click', function () {
 
   return false;
 });
+
+// work sort
+$('.filters-list a').on('click', function () {
+  var filter = $(this).attr('data-filter');
+  $('.project').hide();
+  $(filter).show();
+
+  $('.filters-list a').removeClass('selected');
+  $(this).addClass('selected');
+  return false;
+});
