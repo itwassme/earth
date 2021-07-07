21f8d0e743025f4ef07e1545f16d92bb1805c64d
Emily <itwassme@gmail.com>
2021-07-07 10:14:03 +0900

    added facebook

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-const burger = document.querySelector('.burger');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-document.addEventListener('click', function () {
-  if (mobileMenu.classList.contains('open')) {
-    mobileMenu.classList.remove('open');
-  } else {
-    mobileMenu.classList.add('open');
-  }
-});
-
-$('.menu-toggle').on('click', function () {
+$('a.menu-toggle').on('click', function () {
   $('.mobile-menu').toggleClass('open');
   $(this).text($(this).html() == 'Menu' ? 'Close' : 'Menu');
 
